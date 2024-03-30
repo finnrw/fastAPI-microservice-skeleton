19ce345e34d843dca6b37c333d0c2346ff2d845b
Finn Williams <finnreg47@gmail.com>
2024-03-30 17:33:42 -0400

    new default test structure

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,8 @@
 from fastapi.testclient import TestClient
-from httpx import AsyncClient
-import pytest
 from app.main import app
 
-client = TestClient(app)
-
-@pytest.mark.asyncio
-async def test_read_main():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+def test_read_main():
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"Hello": "World"}
